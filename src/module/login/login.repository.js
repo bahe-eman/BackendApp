f5bd213f8370e0d324e39dd017b6c812150012bf
Hilmi <hilmi@amtiss.com>
2023-11-18 21:17:15 +0700

    fixed login

--- src/module/login/login.repository.js
+++ src/module/login/login.repository.js
@@ -1,15 +1,14 @@
 const prisma = require("../../db/index");
 
-
 const checkUser = async (newData) => {
-    const users = await prisma.user.findFirst({
-      where: {
-        emailUser: newData,
-      },
-    });
-    return users;
-  };
+  const users = await prisma.user.findFirst({
+    where: {
+      emailUser: newData.emailUser,
+    },
+  });
+  return users;
+};
 
 module.exports = {
-    checkUser
-}
+  checkUser,
+};
